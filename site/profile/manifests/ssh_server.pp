e414526958cc369192602cc7ae3f4d90b788db57
Marcel Chacon <machacon@vrbo.com>
2019-10-16 15:20:47 -0500

    Adding host entries for the agent_nodes

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -8,8 +8,8 @@ class profile::ssh_server {
   }
   ssh_authorized_key {
     ensure => present,
-    user   => 'root'
-    type   => 'ssh-rsa'
-    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDg3OV8ydKnhPq3fTtxaPWfeCagE6IFWMvksT/dWe+HaIVsNk2X5EMzP6gfa3C4nbFB++wnsIMMdFTtekzDW9p6hoB2qFVJXImkf6YjxJGHanx/EBJkdYzE1vjiqaIJSkeR6sJywXLmajy5cSCGCYVGhA7m1EumgbRq4TuWDcikS+gpQ8RKx22YNFBN516bW63AawfNg1EI8dZTXubvrI49GF+6wFPO3dFfWW70splaqZc1qVVYH5q14HzSno40W9WcLpQxRKzZ8ugIqwi+1RGHegruL0TunX2T/n/A1escO8ROWkQduIZa/XHWyxgL45Ypt89IVE6x/4TlnujqB/8H'
+    user   => 'root',
+    type   => 'ssh-rsa',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDg3OV8ydKnhPq3fTtxaPWfeCagE6IFWMvksT/dWe+HaIVsNk2X5EMzP6gfa3C4nbFB++wnsIMMdFTtekzDW9p6hoB2qFVJXImkf6YjxJGHanx/EBJkdYzE1vjiqaIJSkeR6sJywXLmajy5cSCGCYVGhA7m1EumgbRq4TuWDcikS+gpQ8RKx22YNFBN516bW63AawfNg1EI8dZTXubvrI49GF+6wFPO3dFfWW70splaqZc1qVVYH5q14HzSno40W9WcLpQxRKzZ8ugIqwi+1RGHegruL0TunX2T/n/A1escO8ROWkQduIZa/XHWyxgL45Ypt89IVE6x/4TlnujqB/8H',
   }
 }
