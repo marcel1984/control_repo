31ee3f470d8d14ec64f21899091e15f2da938736
Marcel Chacon <machacon@vrbo.com>
2019-10-16 16:36:44 -0500

    Using variables, templates and parameters for install_dir

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,24 +1,31 @@
-class minecraft {
-  file {'/opt/minecraft':
+class minecraft (
+  $url = 'https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar',
+  $install_dir = '/opt/minecraft',
+  ){
+  file {$install_dir:
     ensure => directory,
   }
-  file { '/opt/minecraft/server.jar':
+  file { "${install_dir}/server.jar":
     ensure => 'file',
-    source => 'https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar'
+    source => $url,
+    before => Service['minecraft'],
   }
   package { 'java':
     ensure => 'present',
   }
-  file { '/opt/minecraft/eula.txt':
+  file { "${install_dir}/eula.txt":
     ensure  => file,
     content => 'eula=true',
   }
   file { '/etc/systemd/system/minecraft.service':
-    ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.service'
+    ensure  => file,
+    content => epp('minecraft/minecraft.service',{
+      install_dir => $install_dir,
+    })
   }
   service { 'minecraft':
     ensure => 'running',
     enable => true,
+    require => [Package['java'],File["${install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']],
   }
 }
